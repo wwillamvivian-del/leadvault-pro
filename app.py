0158374efdf0d8ebd8e8034ef5a89328361070d9
Your Name <your-email@example.com>
2026-04-05 08:54:00 -0400

    Fixing terminal paste error

--- app.py
+++ app.py
@@ -3,18 +3,15 @@ import os
 
 app = Flask(__name__)
 
-stats = {"visits": 0, "clicks": 0}
-
 @app.route('/')
 def home():
-    stats["visits"] += 1
     return render_template('index.html')
 
 @app.route('/click')
 def click():
-    stats["clicks"] += 1
     return "<h3>Redirecting to Secure Payment Vault...</h3>"
 
 if __name__ == '__main__':
-    # We use port 10000 specifically for Render
-    app.run(host='0.0.0.0', port=10000)
+    # Use port 10000 for Render
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
